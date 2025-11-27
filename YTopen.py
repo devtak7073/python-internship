3089df4bf08257f9ac5c0afef820c71f83c7fefe
devtak7073 <devtak7073@gmail.com>
2025-11-27 14:04:55 +0600

    Add files via upload

---
+++ YTopen.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+import pywhatkit
+mic = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Start speaking...")
+    voice = mic.listen(source)
+    text = mic.recognize_google(voice)
+    print("Now Opening ", text)
+    
+pywhatkit.playonyt(text)
