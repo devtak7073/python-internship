01599933474e6f167fd69b4044a729d3de848e8a
devtak7073 <devtak7073@gmail.com>
2025-11-29 15:51:17 +0600

    Add files via upload

---
+++ AI-chat-bot.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import google.generativeai as genai
+key = "Your API Key here"
+genai.configure(api_key=key)
+model = genai.GenerativeModel("gemini-2.5-flash")
+st.header("AI Bot")
+st.write("You can ask me whatever you wnats to ask")
+user = st.chat_input("Heii, How are you Doin'")
+
+if user:
+    st.chat_message("user")
+    st.markdown(user)
+    
+    st.chat_message("Assistant")
+    response = model.generate_content(user)
+    st.markdown(response.text)
+    
